47ce6e73abe8cfd4c9124d31d781e1cd8a8a6e6b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-04-20 16:54:36 +0200

    Initial commit with Hastlayer sample code for ProgSCon

---
+++ PrimeCalculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hast.Transformer.SimpleMemory;
+
+namespace Hast.Samples.Psc
+{
+    public class PrimeCalculator
+    {
+        public const int IsPrimeNumber_InputUInt32Index = 0;
+        public const int IsPrimeNumber_OutputBooleanIndex = 0;
+        public const int ArePrimeNumbers_InputUInt32CountIndex = 0;
+        public const int ArePrimeNumbers_InputUInt32sStartIndex = 1;
+        public const int ArePrimeNumbers_OutputUInt32sStartIndex = 1;
+
+    
+        public virtual void IsPrimeNumber(SimpleMemory memory)
+        {
+            var number = memory.ReadUInt32(IsPrimeNumber_InputUInt32Index);
+            memory.WriteBoolean(IsPrimeNumber_OutputBooleanIndex, IsPrimeNumber(number));
+        }
+
+        public virtual void ArePrimeNumbers(SimpleMemory memory)
+        {
+            uint numberCount = memory.ReadUInt32(ArePrimeNumbers_InputUInt32CountIndex);
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                uint number = memory.ReadUInt32(ArePrimeNumbers_InputUInt32sStartIndex + i);
+                memory.WriteBoolean(ArePrimeNumbers_OutputUInt32sStartIndex + i, IsPrimeNumber(number));
+            }
+        }
+
+
+        private bool IsPrimeNumber(uint number)
+        {
+            uint factor = number / 2;
+
+            for (uint i = 2; i <= factor; i++)
+            {
+                if ((number % i) == 0) return false;
+            }
+
+            return true;
+        }
+    }
+
+
+    public static class PrimeCalculatorExtensions
+    {
+        public static bool IsPrimeNumber(this PrimeCalculator primeCalculator, uint number)
+        {
+            var memory = new SimpleMemory(1);
+            memory.WriteUInt32(PrimeCalculator.IsPrimeNumber_InputUInt32Index, number);
+            primeCalculator.IsPrimeNumber(memory);
+            return memory.ReadBoolean(PrimeCalculator.IsPrimeNumber_OutputBooleanIndex);
+        }
+
+        public static bool[] ArePrimeNumbers(this PrimeCalculator primeCalculator, uint[] numbers)
+        {
+            var memory = new SimpleMemory(numbers.Length + 1);
+
+            memory.WriteUInt32(PrimeCalculator.ArePrimeNumbers_InputUInt32CountIndex, (uint)numbers.Length);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                memory.WriteUInt32(PrimeCalculator.ArePrimeNumbers_InputUInt32sStartIndex + i, numbers[i]);
+            }
+
+            primeCalculator.ArePrimeNumbers(memory);
+
+
+            var output = new bool[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                output[i] = memory.ReadBoolean(PrimeCalculator.ArePrimeNumbers_OutputUInt32sStartIndex + i);
+            }
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Schedulers;
+using Hast.Common.Configuration;
+using Hast.Common.Models;
+using Hast.Layer;
+using Hast.VhdlBuilder.Representation;
+
+namespace Hast.Samples.Psc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                using (var hastlayer = Hast.Xilinx.HastlayerFactory.Create())
+                {
+                    #region Configuration
+                    hastlayer.ExecutedOnHardware += (sender, e) =>
+                    {
+                        Console.WriteLine(
+                            "Executing " +
+                            e.MemberFullName +
+                            " on hardware took " +
+                            e.HardwareExecutionInformation.HardwareExecutionTimeMilliseconds +
+                            "ms (net) " +
+                            e.HardwareExecutionInformation.FullExecutionTimeMilliseconds +
+                            " milliseconds (all together)");
+                    };
+
+
+                    var hardwareConfiguration = new HardwareGenerationConfiguration();
+                    hardwareConfiguration.PublicHardwareMemberNamePrefixes.Add("Hast.Samples.Psc.PrimeCalculator");
+                    var hardwareRepresentation = await hastlayer.GenerateHardware(
+                        new[]
+                            {
+                                typeof(Program).Assembly
+                            },
+                        hardwareConfiguration);
+
+                    var stopwatch = new Stopwatch();
+                    #endregion
+
+
+                    var primeCalculator = await hastlayer
+                        .GenerateProxy(hardwareRepresentation, new PrimeCalculator());
+
+
+                    #region Basic samples
+                    var isPrime = primeCalculator.IsPrimeNumber(15);
+                    var isPrime2 = primeCalculator.IsPrimeNumber(13);
+                    var arePrimes = primeCalculator.ArePrimeNumbers(new uint[] { 15, 493, 2341, 99237 });
+                    var arePrimes2 = primeCalculator.ArePrimeNumbers(new uint[] { 13, 493 });
+                    Debugger.Break();
+                    #endregion
+
+                    #region Generating some big numbers
+                    var numberCount = 4000;
+                    var numbers = new uint[numberCount];
+                    for (uint i = (uint)(uint.MaxValue - numberCount); i < uint.MaxValue; i++)
+                    {
+                        numbers[i - (uint.MaxValue - numberCount)] = (uint)i;
+                    }
+                    #endregion
+
+                    #region Crunching big numbers
+                    stopwatch.Restart();
+                    var arePrimesOnCpu = new PrimeCalculator().ArePrimeNumbers(numbers.Take(20).ToArray());
+                    StopAndWriteTime(stopwatch);
+                    Debugger.Break();
+                    stopwatch.Restart();
+                    primeCalculator.ArePrimeNumbers(numbers.Take(20).ToArray());
+                    StopAndWriteTime(stopwatch);
+                    Debugger.Break();
+                    stopwatch.Restart();
+                    var arePrimes3 = primeCalculator.ArePrimeNumbers(numbers);
+                    StopAndWriteTime(stopwatch);
+                    Debugger.Break();
+                    #endregion
+
+                    #region Sequential launch
+                    stopwatch.Restart();
+                    var sequentialLaunchArePrimes = new bool[10][];
+                    for (uint i = 0; i < 10; i++)
+                    {
+                        sequentialLaunchArePrimes[i] = primeCalculator.ArePrimeNumbers(numbers);
+                    }
+                    StopAndWriteTime(stopwatch);
+                    Debugger.Break();
+                    #endregion
+
+                    #region Parallel launch
+                    var taskScheduler = new QueuedTaskScheduler(2);
+                    stopwatch.Restart();
+                    var parallelLaunchedIsPrimeTasks = new List<Task<bool[]>>();
+                    for (uint i = 0; i < 10; i++)
+                    {
+                        parallelLaunchedIsPrimeTasks.Add(Task.Factory.StartNew(() =>
+                            primeCalculator.ArePrimeNumbers(numbers),
+                            new CancellationToken(),
+                            TaskCreationOptions.None,
+                            taskScheduler));
+                    }
+                    var parallelLaunchedArePrimes = await Task.WhenAll(parallelLaunchedIsPrimeTasks);
+                    StopAndWriteTime(stopwatch);
+                    Debugger.Break();
+                    #endregion
+
+                    #region Looking at VHDL
+                    File.WriteAllText(@"C:\HastlayerSample.vhd", ToVhdl(hardwareRepresentation.HardwareDescription));
+                    Debugger.Break();
+                    #endregion
+                }
+            }).Wait();
+        }
+
+
+        private static string ToVhdl(IHardwareDescription hardwareDescription)
+        {
+            return ((Hast.Transformer.Vhdl.Models.VhdlHardwareDescription)hardwareDescription)
+                .Manifest.TopModule.ToVhdl(new VhdlGenerationOptions { FormatCode = true, NameShortener = VhdlGenerationOptions.SimpleNameShortener });
+        }
+
+        private static void StopAndWriteTime(Stopwatch stopwatch)
+        {
+            stopwatch.Stop();
+            Console.WriteLine("===========");
+            Console.WriteLine("Ellapsed milliseconds: " + stopwatch.ElapsedMilliseconds);
+            Console.WriteLine();
+        }
+    }
+}
